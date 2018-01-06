bf020e01e0b66ee331f8f62a05367ecf053f1d19
vadrianc <vadrianc@users.noreply.github.com>
2018-01-06 10:04:11 +0200

    Use the "smart" upgrade command. Echo message for each command.

--- os-upgrade.sh
+++ os-upgrade.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "Clear out the local repository of retrieved package files"
 sudo apt-get clean
+
+echo "Remove packages automatically installed that are no longer needed"
 sudo apt-get autoremove
+
+echo "Resynchronize the package index files from their sources"
 sudo apt-get update
-sudo apt-get upgrade
+
+echo "Smart upgrade"
+sudo apt-get dist-upgrade --assume-yes

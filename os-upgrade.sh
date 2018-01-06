dfe2ffb998f1d901374ca6401e22c3ad07ea4662
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2018-01-06 09:51:25 +0000

    Run cleanup commands before and after upgrade

--- os-upgrade.sh
+++ os-upgrade.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-echo "Clear out the local repository of retrieved package files"
-sudo apt-get clean
+cleanup()
+{
+ echo -e "\n* Clear out the local repository of retrieved package files *"
+ sudo apt-get clean
 
-echo "Remove packages automatically installed that are no longer needed"
-sudo apt-get autoremove
+ echo -e "\n* Remove packages automatically installed that are no longer needed *"
+ sudo apt-get autoremove --assume-yes
+}
 
-echo "Resynchronize the package index files from their sources"
-sudo apt-get update
+upgrade()
+{
+ echo -e "\n* Resynchronize the package index files from their sources *"
+ sudo apt-get update
 
-echo "Smart upgrade"
-sudo apt-get dist-upgrade --assume-yes
+ echo -e "\n* Smart upgrade *"
+ sudo apt-get dist-upgrade --assume-yes
+}
+
+cleanup
+upgrade
+cleanup
